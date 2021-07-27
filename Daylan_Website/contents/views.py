cc620dd8ef8cef24c10cafb5df5d75fb59996c1a
Daylan Quinn <daylanq10@gmail.com>
2021-07-27 16:12:28 -0500

    Made Progress

--- Daylan_Website/contents/views.py
+++ Daylan_Website/contents/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 # Create your views here.
 def homepage(request):
     # messages.info(request, "Logged out successfully")  # Doesnt work will figure out
-    return render(request, 'Homepage.html')
+    return render(request, 'homepage.html')
